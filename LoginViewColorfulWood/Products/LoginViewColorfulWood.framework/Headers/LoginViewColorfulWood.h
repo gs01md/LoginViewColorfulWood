2135ef97897ca7bc256e737256ac70aa88544e88
gs01md <gshzyfx@163.com>
2016-05-04 19:58:21 +0800

    界面测试

---
+++ LoginViewColorfulWood/Products/LoginViewColorfulWood.framework/Headers/LoginViewColorfulWood.h
@@ -0,0 +1,20 @@
+//
+//  LoginViewColorfulWood.h
+//  LoginViewColorfulWood
+//
+//  Created by Simon on 16/5/4.
+//  Copyright © 2016年 gaoshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoginViewColorfulWood.
+FOUNDATION_EXPORT double LoginViewColorfulWoodVersionNumber;
+
+//! Project version string for LoginViewColorfulWood.
+FOUNDATION_EXPORT const unsigned char LoginViewColorfulWoodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginViewColorfulWood/PublicHeader.h>
+
+
+//
---
+++ TestLoginViewColorfulWood/TestLoginViewColorfulWood/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLoginViewColorfulWood
+//
+//  Created by Simon on 16/5/4.
+//  Copyright © 2016年 gaoshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
